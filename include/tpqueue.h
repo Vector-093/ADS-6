d65d1300f87b58141611297398cbf24692f146c9
Vector-093 <motleo2007@gmail.com>
2026-05-10 17:02:27 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,70 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
 struct SYM {
   char ch;
   int prior;
+};
+
+template<typename T>
+class TPQueue {
+ private:
+  struct Node {
+    T data;
+    Node* next;
+  };
+
+  Node* head;
+
+ public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+
+  void push(const T& item) {
+    Node* newNode = new Node;
+    newNode->data = item;
+    newNode->next = nullptr;
+
+    if (isEmpty() || item.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next != nullptr &&
+             current->next->data.prior >= item.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      throw "Queue is empty";
+    }
+    Node* temp = head;
+    T result = head->data;
+    head = head->next;
+    delete temp;
+    return result;
+  }
+
+  T front() const {
+    if (isEmpty()) {
+      throw "Queue is empty";
+    }
+    return head->data;
+  }
+
+  bool isEmpty() const {
+    return head == nullptr;
+  }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
